16bb3efec9453ee26ea9b293b5a2018e41db38ca
“kellylimmm” <kellylim.06@gmail.com>
2019-10-24 18:27:17 +0800

    MVP

--- views/showTransaction.jsx
+++ views/showTransaction.jsx
@@ -13,15 +13,15 @@ class ShowTransaction extends React.Component {
 
             return (
                 <div>
-                    <p>
-                        {el.name}
-                    </p>
-                    <p>
-                        {el.amount}
-                    </p>
-                    <form action={url} method="POST">
-                    <button> Delete </button>
-                    </form>
+                    <tr>
+                          <th scope="row"></th>
+                          <td>{el.name}</td>
+                          <td>{el.amount}</td>
+                          <td><form action={url} method="POST">
+                          <button> Delete </button>
+                          </form>
+                          </td>
+                    </tr>
                 </div>
             )
 
@@ -29,14 +29,34 @@ class ShowTransaction extends React.Component {
 
     return (
         <html>
+            <head>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+                <link rel="stylesheet" type="text/css" href="/styles.css"/>
+            </head>
             <body>
-                <h1> Expenses: </h1>
 
+                <h1 className="row justify-content-center"> Expenses: </h1>
 
-                {debtor}
+                <table class="table">
+                      <thead>
+                            <tr>
+                              <th scope="col"></th>
+                              <th scope="col">Name</th>
+                              <th scope="col">Amount</th>
+                              <th scope="col">Delete</th>
+                            </tr>
+                      </thead>
 
+                      <tbody>
+                    {debtor}
+                      </tbody>
+                </table>
 
-                <a href="/transaction/add"><button>Add Transaction</button></a>
+                <a href="/transaction/add" className="row justify-content-center"><button>Add Transaction</button></a>
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
             </body>
         </html>
