ca79f5c6ba45bb20d51fe23defe28421c214d3ce
“kellylimmm” <kellylim.06@gmail.com>
2019-10-30 13:11:16 +0800

    Update

--- index.js
+++ index.js
@@ -11,16 +11,12 @@ var SALT = "payuplah";
 
 // const pool = new pg.Pool(config);
 
-const pool = new pg.Pool(configs);
 
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
-});
 
 //====================
 
 const url = require('url');
-
+var configs;
 
 if( process.env.DATABASE_URL ){
 
@@ -28,7 +24,7 @@ if( process.env.DATABASE_URL ){
   const auth = params.auth.split(':');
 
   //make the configs object
-  var configs = {
+  configs = {
     user: auth[0],
     password: auth[1],
     host: params.hostname,
@@ -40,14 +36,18 @@ if( process.env.DATABASE_URL ){
 }else{
 
   //otherwise we are on the local network
-  var configs = {
+  configs = {
       user: 'kellylim',
       host: '127.0.0.1',
       database: 'payup_db',
       port: 5432
   };
 }
+const pool = new pg.Pool(configs);
 
+pool.on('error', function (err) {
+  console.log('idle client error', err.message, err.stack);
+});
 //this is the same
 
 
